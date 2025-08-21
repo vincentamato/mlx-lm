538f0ca627e8c9ea819b77512ee67ca4433b8054
Goekdeniz-Guelmez <gulmezdeniz1999@gmail.com>
2025-08-21 20:55:22 +0200

    udpdates

--- mlx_lm/models/mamba2-torch-mlx.py
+++ mlx_lm/models/mamba2-torch-mlx.py
@@ -11,7 +11,7 @@ print(tokenizer.batch_decode(out))
 """
 
 python -m mlx_lm.generate --model AntonV/mamba2-130m-hf --prompt "Hey," --temp 0.8
-python -m mlx_lm.generate --model AntonV/mamba2-1.3b-hf  --prompt "Einstein was a" --temp 0.8
+python -m mlx_lm.generate --model AntonV/mamba2-1.3b-hf  --prompt "Hey," --temp 0.8
 python -m mlx_lm.generate --model mlx-community/Mamba-Codestral-7B-v0.1-4bit --prompt "<s>[SYSTEM_PROMPT]You are a helpful assistant.[/SYSTEM_PROMPT][INST]Write a function in python[/INST]"
 python -m mlx_lm.convert --hf-path
 """
